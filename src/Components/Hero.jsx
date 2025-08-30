9e14d885952db3ca398eaa1ef3ab761ac0a6364c
Muhammad Qasim <muhammadqqaasim@gmail.com>
2025-08-31 03:14:53 +0500

    Initial

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import assets from '../assets/assets'
-import TrustedBy from './trustedBy'
+import TrustedBy from './TrustedBy'
 import { useGSAP } from '@gsap/react'
 import gsap from 'gsap'
 import { SplitText } from "gsap/SplitText";
